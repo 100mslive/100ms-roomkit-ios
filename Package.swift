a1479555e46ce5136df436ecb9789acb10ce10c9
pawan-100ms <pawan@100ms.live>
2023-09-07 12:51:28 +0400

    Removed commented lines from package file

--- Package.swift
+++ Package.swift
@@ -15,9 +15,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-//        .package(url: "https://github.com/100mslive/100ms-ios-analytics-sdk", from: "0.0.2"),
+        .package(url: "https://github.com/100mslive/100ms-roomkit-models-ios", branch:"development"),
         .package(url: "https://github.com/100mslive/Popovers", branch:"main"),
-        .package(url: "https://github.com/100mslive/100ms-roomkit-models-ios", branch:"development")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -32,21 +31,6 @@ let package = Package(
                 .product(name: "WebRTC", package: "100ms-roomkit-models-ios")
                 ]
         ),
-//        .binaryTarget(
-//            name: "HMSSDK",
-//            url: "https://github.com/100mslive/100ms-ios-sdk/releases/download/0.9.10/HMSSDK.xcframework.zip",
-//            checksum: "4263e4842d189b16131ea22365f909bb8228df1c0428aff83d49fdc321ec5970"
-//        ),
-//        .binaryTarget(
-//            name: "HMSAnalyticsSDK",
-//            url: "https://github.com/100mslive/100ms-ios-analytics-sdk/releases/download/0.0.2/HMSAnalyticsSDK.xcframework.zip",
-//            checksum: "40229908576cac8afab7f9ba8b3bd9b1408f97f7bff63f83dca5b4f60f4378f0"
-//        ),
-//        .binaryTarget(
-//            name: "WebRTC",
-//            url: "https://github.com/100mslive/webrtc-ios/releases/download/1.0.5116/WebRTC.xcframework.zip",
-//            checksum: "5f38579bb743b089d95017fa56dc76f8e3e440dbdd56061db04c26448262cfee"
-//        ),
         .binaryTarget(
             name: "HMSHLSPlayerSDK",
             url: "https://github.com/100mslive/100ms-ios-hls-sdk/releases/download/0.0.2/HMSHLSPlayerSDK.xcframework.zip",
@@ -56,12 +40,5 @@ let package = Package(
             name: "Lottie",
             url: "https://github.com/airbnb/lottie-ios/releases/download/4.2.0/Lottie.xcframework.zip",
             checksum: "4db3dee208f6213e5c1681f2314c7ed96d466d9b9adfe5cd0030309515075443"),
-//        .target(name: "HMSRoomKitDependencies", dependencies: [
-//            "HMSHLSPlayerSDK", "Lottie",
-//            .product(name: "HMSRoomModels", package: "100ms-roomkit-models-ios"),
-//            .product(name: "HMSSDK", package: "100ms-roomkit-models-ios"),
-//            .product(name: "HMSAnalyticsSDK", package: "100ms-roomkit-models-ios"),
-//            .product(name: "WebRTC", package: "100ms-roomkit-models-ios")
-//            ], path: "dependencies")
     ]
 )
