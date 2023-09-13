bddecca8cfb8e8e6307aa3aa77cca3fb2654b5a2
pawan-100ms <pawan@100ms.live>
2023-09-13 11:41:46 +0400

    Added HMSRoomKitDevelopmentProject Xcode project
    With HMSRoomKitDevelopmentProject, you can see SwiftUI preview of files in RoomKit and make changes to contribute to repo

---
+++ HMSRoomKitDevelopmentProject/HMSRoomKitDevelopmentProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HMSRoomKitDevelopmentProject
+//
+//  Created by Pawan Dixit on 13/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
