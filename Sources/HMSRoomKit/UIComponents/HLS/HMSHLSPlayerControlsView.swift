1ace493d32c621c20f6295e1f81e746cee82071a
pawan-100ms <pawan@100ms.live>
2024-01-25 13:35:46 +0400

    Showing subtitle button only when subtitles are enabled.

--- Sources/HMSRoomKit/UIComponents/HLS/HMSHLSPlayerControlsView.swift
+++ Sources/HMSRoomKit/UIComponents/HLS/HMSHLSPlayerControlsView.swift
@@ -1,6 +1,6 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by Pawan Dixit on 12/11/23.
 //
@@ -16,11 +16,13 @@ struct HMSHLSPlayerControlsView: View {
     
     @State private var isPopoverPresented = false
     
-//    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    //    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     @State private var refresh = false
     
     @Binding var isMaximized: Bool
+    
+    @State var isSubtitleToggleShown = false
     
     var body: some View {
         Color.clear
@@ -46,27 +48,33 @@ struct HMSHLSPlayerControlsView: View {
                 
                 HStack(spacing: 20) {
                     
-                    Image(assetName: "subtitle-toggle")
-                        .resizable()
-                        .foreground(.white)
-                        .frame(width: 32, height: 32)
-                        .onTapGesture {
-                            let player = player._nativePlayer
-                            guard let playerItem = player.currentItem else {return }
-                            
-                            guard let availableSubtitleTracks = playerItem.asset.mediaSelectionGroup(forMediaCharacteristic: .legible) else { return }
-                            
-                            // subtitle are available in media
-                            
-                            if let _ = playerItem.currentMediaSelection.selectedMediaOption(in: availableSubtitleTracks) {
-                                // subtitle is selected, remove it
-                                playerItem.select(nil, in: availableSubtitleTracks)
+                    if isSubtitleToggleShown {
+                        
+                        Image(assetName: "subtitle-toggle")
+                            .resizable()
+                            .foreground(.white)
+                            .frame(width: 32, height: 32)
+                            .onTapGesture {
+                                let player = player._nativePlayer
+                                guard let playerItem = player.currentItem else {return }
+                                
+                                guard let availableSubtitleTracks = playerItem.asset.mediaSelectionGroup(forMediaCharacteristic: .legible) else { return }
+                                
+                                // subtitle are available in media
+                                
+                                if let _ = playerItem.currentMediaSelection.selectedMediaOption(in: availableSubtitleTracks) {
+                                    // subtitle is selected, remove it
+                                    playerItem.select(nil, in: availableSubtitleTracks)
+                                }
+                                else {
+                                    // subtitle is not selected, set the first available subtitle
+                                    if let firstSubtitle = availableSubtitleTracks.options.first(where: {$0.mediaType == .subtitle}) {
+                                        
+                                        playerItem.select(firstSubtitle, in: availableSubtitleTracks)
+                                    }
+                                }
                             }
-                            else {
-                                // subtitle is not selected, set the first available subtitle
-                                playerItem.select(availableSubtitleTracks.options.first, in: availableSubtitleTracks)
-                            }
-                        }
+                    }
                     
                     Image(assetName: "gear-icon")
                         .resizable()
@@ -85,26 +93,50 @@ struct HMSHLSPlayerControlsView: View {
                 }
                 .edgesIgnoringSafeArea(.all)
             }
-//            .overlay(alignment: .bottomTrailing, content: {
-//                VStack {
-//                    if refresh {
-//                        Text("preferredPeakBitRate: \(player._nativePlayer.currentItem?.preferredPeakBitRate ?? -1)")
-//                        Text("bitrate: \(player.statMonitor.bitrate)")
-//                        Text("estimatedBandwidth: \(player.statMonitor.estimatedBandwidth)")
-//                        Text("width: \(player.statMonitor.videoSize.width), height: \(player.statMonitor.videoSize.height)")
-//                    }
-//                    else {
-//                        Text("preferredPeakBitRate: \(player._nativePlayer.currentItem?.preferredPeakBitRate ?? -1)")
-//                        Text("bitrate: \(player.statMonitor.bitrate)")
-//                        Text("estimatedBandwidth: \(player.statMonitor.estimatedBandwidth)")
-//                        Text("width: \(player.statMonitor.videoSize.width), height: \(player.statMonitor.videoSize.height)")
-//                    }
-//                }
-//                .foreground(.white)
-//            })
-//            .onReceive(timer) { _ in
-//                refresh.toggle()
-//            }
+            .onAppear() {
+                let player = player._nativePlayer
+                guard let playerItem = player.currentItem else {
+                    isSubtitleToggleShown = false
+                    return
+                }
+                
+                guard let availableSubtitleTracks = playerItem.asset.mediaSelectionGroup(forMediaCharacteristic: .legible) else {
+                    isSubtitleToggleShown = false
+                    return
+                }
+                
+                guard let firstOption = availableSubtitleTracks.options.first else {
+                    isSubtitleToggleShown = false
+                    return
+                }
+                
+                if firstOption.mediaType == .subtitle {
+                    isSubtitleToggleShown = true
+                }
+                else {
+                    isSubtitleToggleShown = false
+                }
+            }
+        //            .overlay(alignment: .bottomTrailing, content: {
+        //                VStack {
+        //                    if refresh {
+        //                        Text("preferredPeakBitRate: \(player._nativePlayer.currentItem?.preferredPeakBitRate ?? -1)")
+        //                        Text("bitrate: \(player.statMonitor.bitrate)")
+        //                        Text("estimatedBandwidth: \(player.statMonitor.estimatedBandwidth)")
+        //                        Text("width: \(player.statMonitor.videoSize.width), height: \(player.statMonitor.videoSize.height)")
+        //                    }
+        //                    else {
+        //                        Text("preferredPeakBitRate: \(player._nativePlayer.currentItem?.preferredPeakBitRate ?? -1)")
+        //                        Text("bitrate: \(player.statMonitor.bitrate)")
+        //                        Text("estimatedBandwidth: \(player.statMonitor.estimatedBandwidth)")
+        //                        Text("width: \(player.statMonitor.videoSize.width), height: \(player.statMonitor.videoSize.height)")
+        //                    }
+        //                }
+        //                .foreground(.white)
+        //            })
+        //            .onReceive(timer) { _ in
+        //                refresh.toggle()
+        //            }
     }
 }
 
