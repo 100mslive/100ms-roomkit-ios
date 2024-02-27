d9dc15dba5740c902029dd5c3da1f3ddb332a8a8
pawan-100ms <pawan@100ms.live>
2024-02-27 14:32:03 +0400

    Added noise cancellation feature and UI in Prebuilt

--- Sources/HMSRoomKit/UIComponents/Conferencing/Internal/HMSOptionSheetView.swift
+++ Sources/HMSRoomKit/UIComponents/Conferencing/Internal/HMSOptionSheetView.swift
@@ -125,6 +125,14 @@ struct HMSOptionSheetView: View {
                                 }
                         }
                     }
+                    
+                    if roomModel.isNoiseCancellationAvailable {
+                        HMSSessionMenuButton(text: roomModel.isNoiseCancellationEnabled ? "Noise Reduced" : "Reduce Noise", image: "noise-cancellation", highlighted: (roomModel.isNoiseCancellationEnabled))
+                            .onTapGesture {
+                                try? roomModel.toggleNoiseCancellation()
+                                dismiss()
+                            }
+                    }
                 }
                 .padding(.bottom)
             }
